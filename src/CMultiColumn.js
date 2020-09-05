4b008d95ef1d37f4400ee174b682e0f9e39633f4
Stanislav Ivanov <ivstdm@gmail.com>
2020-09-05 23:05:46 +0300

    CMultiColumn select-like columns support

--- src/CMultiColumn.js
+++ src/CMultiColumn.js
@@ -27,10 +27,35 @@ class CMultiColumn extends CColumn {
      * @param {Boolean} [options.no_search] Disable search field by this column.
      * @param {List} [options.search_columns] List of columns used for search (all columns if empty).
      * @param {int} [options.labels_width] Label width.
+     * @param {Object} [options.columns_options] For select-like columns - options list
+     * @param {Object} [options.columns_endpoints] For select-like columns - remote options list url
+     * @param {Object} [options.columns_params] For select-like columns - remote options list params
      */
 
     constructor(table, record, options = {}) {
         super(table, record, options);
+
+        if (! ('columns_options' in this.options)){
+            this.options.columns_options = {};
+        }
+        if (! ('columns_endpoints' in this.options)){
+            this.options.columns_endpoints = {};
+        }
+        if (! ('columns_params' in this.options)){
+            this.options.columns_params = {};
+        }
+
+        for (var i in this.options.columns_endpoints){
+            this.table.load_from_options_cache(this.options.columns_endpoints[i], this.options.columns_params[i], $.proxy(this.fill_options, this, i));
+        }
+
+    }
+
+    fill_options(colname, options){
+        this.options.columns_options[colname] = options;
+        if (this.cell_elem != null){
+            this.build_cell(this.cell_elem);
+        }
     }
 
     /**
@@ -42,6 +67,7 @@ class CMultiColumn extends CColumn {
 
     build_cell(elem){
         super.build_cell(elem);
+        this.cell_elem = elem;
 
         var dt_style = '';
         if(typeof(this.options.labels_width) != "undefined"){
@@ -50,7 +76,20 @@ class CMultiColumn extends CColumn {
 
         var text = '';
         for (var i in this.options.columns){
-            text += '<dt'+dt_style+'> ' + this.options.labels[i] + ': </dt><dd>' + this.record.record_field(this.options.columns[i]) + '</dd>';
+            var cell_value = this.record.record_field(this.options.columns[i]);
+
+            if(this.options.columns[i] in this.options.columns_options){
+                for (var k in this.options.columns_options[this.options.columns[i]]){
+                    if (this.options.columns_options[this.options.columns[i]][k][0] == cell_value)
+                        cell_value = this.options.columns_options[this.options.columns[i]][k][1];
+                }
+            } else {
+                if(this.options.columns[i] in this.options.columns_endpoints){
+                    cell_value = '';
+                }
+            }
+
+            text += '<dt'+dt_style+'> ' + this.options.labels[i] + ': </dt><dd>' + cell_value + '</dd>';
         }
 
         elem.html('<dl>' + text + '</dl>');
